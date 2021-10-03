01f9703d4e10627ec9a4c08347a4800e3b90c463
carlosgarcial <carlos.garcial@viewnext.com>
2021-10-03 18:21:07 +0200

    cal

--- fundamentos/app/js/calculadora.js
+++ fundamentos/app/js/calculadora.js
@@ -1,7 +1,6 @@
 function suma(a, b) {
     return a + b;
 }
-
 function resta(a, b) {
     return a - b;
 }
@@ -22,7 +21,6 @@ class Calculadora{
         this.pantalla = '0';
         this.resultado = true;
     }
-
 
     ponerdigito(num) {
         if(this.pantalla == '0' || this.resultado){
@@ -52,13 +50,34 @@ class Calculadora{
     }
 
     borrar_digito(){
-        
+        if (this.resultado || this.pantalla.length == 1 || (this.pantalla.length == 2 && this.pantalla.startsWith('-'))) {
+			this.pantalla = '0';
+			this.resultado = true;
+		} else
+        this.pantalla = this.pantalla.substr(0,
+            this.pantalla.length - 1);
     }
 
     borrar_todo(){
-        this.pantalla = ' ';
         this.acum = 0;
-        this.resultado = false;
+        this.op = '+';
+        this.pantalla = '0';
+        this.resultado = true;
+    }
+
+    cambiar_signo(){
+        this.pantalla = (-this.pantalla).toString();
+        this.acum = (-this.acum);
+    }
+
+    poner_coma(){
+        if (this.resultado) {
+            this.pantalla = '0.';
+            this.resultado = false;
+        } else {
+            this.pantalla += '.';
+        }
+        
     }
 
 }
