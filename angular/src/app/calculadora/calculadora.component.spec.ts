cd479a29eddb7a6e2615538a7f5ac7444f7ff23d
carlosgarcial <carlos.garcial@viewnext.com>
2021-10-06 09:33:23 +0200

    calc

--- angular/src/app/calculadora/calculadora.component.spec.ts
+++ angular/src/app/calculadora/calculadora.component.spec.ts
@@ -24,7 +24,7 @@ describe('CalculadoraComponent', () => {
   });
 });
 
-xdescribe('Calculadora',()=>{
+describe('Calculadora',()=>{
   describe('Operaciones_basicas',()=>{
       it('suma', ()=>{
           let a = 2, b = 2;
@@ -61,7 +61,7 @@ xdescribe('Calculadora',()=>{
 
   describe('Calculos',()=>{
       it('poner_digito', () => {
-          let c = new Calculadora();
+          let c = new CalculadoraComponent();
           c.ponerdigito('1');
           expect(c.pantalla).toBe('1');
           c.ponerdigito('0');
@@ -69,7 +69,7 @@ xdescribe('Calculadora',()=>{
       });
 
       it('sumar', () => {
-          let c = new Calculadora();
+          let c = new CalculadoraComponent();
           c.ponerdigito('10');
           c.calcular('+');
           c.ponerdigito('5');
@@ -78,7 +78,7 @@ xdescribe('Calculadora',()=>{
       });
 
       it('restar', () => {
-          let c = new Calculadora();
+          let c = new CalculadoraComponent();
           c.ponerdigito('20');
           c.calcular('-');
           c.ponerdigito('14')
@@ -87,7 +87,7 @@ xdescribe('Calculadora',()=>{
       });
 
       it('multiplicar', () => {
-          let c = new Calculadora();
+          let c = new CalculadoraComponent();
           c.ponerdigito('6');
           c.calcular('*');
           c.ponerdigito('2')
@@ -96,7 +96,7 @@ xdescribe('Calculadora',()=>{
       });
 
       it('dividir', () => {
-          let c = new Calculadora();
+          let c = new CalculadoraComponent();
           c.ponerdigito('12');
           c.calcular('/');
           c.ponerdigito('6')
@@ -106,67 +106,9 @@ xdescribe('Calculadora',()=>{
 
   })
 
-  xdescribe('Calculos_mejorados',()=>{
-      describe('Calculos_Sumar', function () {
-    [[22222, 22222, 44444], [-1, 2, 1], [2, -1, 1], [0, 0, 0],
-    [0.1, 0.2, 0.3], [9.9, 1.3, 11.2]].forEach(caso => {
-      it(`Suma: ${caso[0]} + ${caso[1]} = ${caso[2]}`, function () {
-                  let c = new Calculadora();
-        c.ponerdigito(caso[0])
-        c.calcular('+')
-        c.ponerdigito(caso[1])
-        c.calcular('=')
-        expect(c.pantalla).toBe(caso[2].toString())
-      })
-    });
-  });
-
-  describe('Calculos_Restar', function () {
-    [[22222, 33333, -11111], [-1, 2, -3], [0, 0, 0],
-    [1, 0.9, 0.1]].forEach(caso => {
-      it(`Resta: ${caso[0]} - ${caso[1]} = ${caso[2]}`, function () {
-                  let c = new Calculadora();
-        c.ponerdigito(caso[0])
-        c.calcular('-')
-        c.ponerdigito(caso[1])
-        c.calcular('=')
-        expect(c.pantalla).toBe(caso[2].toString())
-      })
-    });
-  });
-
-  describe('Calculos_Multiplicar', function () {
-    [[10, 5, 50], [1.5, 2, 3], [0, 0, 0], [2, 0, 0],
-    ['Infinity', 0, 'NaN'], ['Infinity', 'NaN', 'Infinity'], ['Infinity', '-Infinity', '-Infinity'],].forEach(caso => {
-      it(`Multiplica: ${caso[0]} * ${caso[1]} = ${caso[2]}`, function () {
-                  let c = new Calculadora();
-        c.ponerdigito(caso[0])
-        c.calcular('*')
-        c.ponerdigito(caso[1])
-        c.calcular('=')
-        expect(c.pantalla).toBe(caso[2].toString())
-      })
-    });
-  });
-
-  describe('Calculos_Dividir', function () {
-    [[10, 5, 2], [1, 3, 0.333333333333333], [0, 0, 'NaN'], [2, 0, 'Infinity'],
-    ['Infinity', 'Infinity', 'NaN']].forEach(caso => {
-      it(`Divide: ${caso[0]} / ${caso[1]} = ${caso[2]}`, function () {
-                  let c = new Calculadora();
-        c.ponerdigito(caso[0])
-        c.calcular('/')
-        c.ponerdigito(caso[1])
-        c.calcular('=')
-        expect(c.pantalla).toBe(caso[2].toString())
-      })
-    });
-  });
-  })
-
   describe('Operaciones_Borrar',()=>{
       it('borrar_digito_positivo', () => {
-          let c = new Calculadora();
+          let c = new CalculadoraComponent();
           c.ponerdigito('3234');
           c.borrar_digito();
           expect(c.pantalla).toBe('323');
@@ -179,7 +121,7 @@ xdescribe('Calculadora',()=>{
       });
 
       it('borrar_digito_negativo', () => {
-          let c = new Calculadora();
+          let c = new CalculadoraComponent();
           c.ponerdigito('-3234');
           c.borrar_digito();
           expect(c.pantalla).toBe('-323');
@@ -195,7 +137,7 @@ xdescribe('Calculadora',()=>{
 
   describe('Operaciones_Cambiar_signos',()=>{
       it('cambiar_signo', () => {
-          let c = new Calculadora();
+          let c = new CalculadoraComponent();
           c.ponerdigito('3');
           c.cambiar_signo();
           expect(c.pantalla).toBe('-3');
@@ -204,7 +146,7 @@ xdescribe('Calculadora',()=>{
 
   describe('Operaciones_Coma',()=>{
       it('poner_coma_positivo', () => {
-          let c = new Calculadora();
+          let c = new CalculadoraComponent();
           c.ponerdigito('3');
           c.poner_coma();
           c.ponerdigito('4');
@@ -212,7 +154,7 @@ xdescribe('Calculadora',()=>{
       });
 
       it('poner_coma_negativo', () => {
-          let c = new Calculadora();
+          let c = new CalculadoraComponent();
           c.ponerdigito('-3');
           c.poner_coma();
           c.ponerdigito('4');
@@ -220,7 +162,7 @@ xdescribe('Calculadora',()=>{
       });
 
       it('empezar_coma', () => {
-          let c = new Calculadora();
+          let c = new CalculadoraComponent();
           c.poner_coma();
           c.ponerdigito('2');
           expect(c.pantalla).toBe('0.2');
