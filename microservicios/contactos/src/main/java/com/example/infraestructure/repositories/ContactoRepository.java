8221542ecf51947c6df753509596b94843001839
[carlosgarcial] <[carlos.garcial@viewnext.com]>
2021-10-25 12:23:22 +0200

    REST Catalogo, demo contacto

--- microservicios/contactos/src/main/java/com/example/infraestructure/repositories/ContactoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.infraestructure.repositories;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.domains.entities.Contacto;
-
-public interface ContactoRepository extends MongoRepository<Contacto, Integer> {
-
-}
